06976e8112c6db5aa4cfa594e0df7bae6c274526
Nathalie Marquesin <nmarquesin@live.com>
2020-06-07 23:44:58 +0100

    Design edit (new question)

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleAddQuestion } from "../actions/getData";
 
+import styles from "./NewQuestion.module.scss";
+
 class NewQuestion extends Component {
   state = {
     optionTwoText: "",
@@ -30,31 +32,51 @@ class NewQuestion extends Component {
     dispatch(handleAddQuestion(optionOneText, optionTwoText));
     this.setState(() => ({
       optionOneText: "",
+      optionTwoText: "",
     }));
   };
   render() {
     return (
-      <div>
-        <h3>New Question</h3>
-        <form onSubmit={this.handleSubmit}>
-          <p>Would you rather...</p>
-          <input
-            type="text"
-            name="optionOneX"
-            id="optionOne"
-            value={this.state.optionOneText}
-            onChange={this.handleChangeTxtOne}
-          />
-          <p>or ...</p>
-          <input
-            type="text"
-            name="optionTwoX"
-            id="optionTwo"
-            value={this.state.optionTwoText}
-            onChange={this.handleChangeTxtTwo}
-          />
-          <input type="submit" value="Submit" />
-        </form>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <div className={styles["card-title"]}>
+            <h3>Create New Question</h3>
+          </div>
+          <div className={styles["card-body"]}>
+            <form className={styles["form-area"]} onSubmit={this.handleSubmit}>
+              <div className={styles["text"]}>Complete the question:</div>
+              <div className={styles["question-prompt"]}>
+                Would you rather ...
+              </div>
+              <div className={styles["input-area"]}>
+                <input
+                  type="text"
+                  name="optionOneX"
+                  id="optionOne"
+                  value={this.state.optionOneText}
+                  onChange={this.handleChangeTxtOne}
+                  className={styles.input}
+                  placeholder="Enter option one here"
+                />
+              </div>
+              <p>or</p>
+              <div className={styles["input-area"]}>
+                <input
+                  type="text"
+                  name="optionTwoX"
+                  id="optionTwo"
+                  value={this.state.optionTwoText}
+                  onChange={this.handleChangeTxtTwo}
+                  className={styles.input}
+                  placeholder="Enter option two here"
+                />
+              </div>
+              <div className={styles.button}>
+                <input className={styles.btn} type="submit" value="Submit" />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
